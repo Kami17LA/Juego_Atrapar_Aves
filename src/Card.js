260c67e14da18bc1923f745f30b136ec94e1a1c1
katherine <katherinebemol17@gmail.com>
2023-06-14 11:41:20 -0500

    Se añadio la función ClickOnCard y se dio estilos al Container

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,14 @@ import casaPajaros from "./images/casaPajaros.png"
 
 export default function Card({bird}){
 
+    const clickOnCard = (e) => {
+        e.currentTarget.style.transform = "rotateY(180deg)";
+       }
+
 	return (
-	<div className='card'>
-		<div className='birds'><img src={bird} alt='bird_image'></img></div>
-		<div className='bird_house'><img src={casaPajaros} alt='bird_house'></img></div>
+	<div className='card' onClick={clickOnCard}>
+		<div className='face birds'><img src={bird} alt='bird_image'></img></div>
+		<div className='face bird_house'><img src={casaPajaros} alt='bird_house'></img></div>
 	</div>
 	)
 }
